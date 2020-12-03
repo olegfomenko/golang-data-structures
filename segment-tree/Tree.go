c6c861a05706cb5b6d5cbdd71825f3fe47851c50
olegfomenko <oleg.fomenko2002@gmail.com>
2020-12-03 20:02:17 +0200

    Refactoring tree code (remove redurant methods)

--- segment-tree/Tree.go
+++ segment-tree/Tree.go
@@ -9,29 +9,19 @@ type Tree interface {
 	Assign(value Value, pos int)
 }
 
-type intTree struct {
+type segmentTree struct {
 	t    []Value
 	size int
 }
 
-type floatTree struct {
-	t    []Value
-	size int
+func (tree segmentTree) Get(l int, r int) Value {
+	return get(tree.t, 1, 0, tree.size-1, l, r)
 }
 
-type stringTree struct {
-	t    []Value
-	size int
+func (tree segmentTree) Assign(value Value, pos int) {
+	update(tree.t, 1, 0, tree.size-1, pos, value)
 }
 
-func (tree intTree) Get(l int, r int) Value    { return get(tree.t, 1, 0, tree.size-1, l, r) }
-func (tree floatTree) Get(l int, r int) Value  { return get(tree.t, 1, 0, tree.size-1, l, r) }
-func (tree stringTree) Get(l int, r int) Value { return get(tree.t, 1, 0, tree.size-1, l, r) }
-
-func (tree intTree) Assign(value Value, pos int)    { update(tree.t, 1, 0, tree.size-1, pos, value) }
-func (tree floatTree) Assign(value Value, pos int)  { update(tree.t, 1, 0, tree.size-1, pos, value) }
-func (tree stringTree) Assign(value Value, pos int) { update(tree.t, 1, 0, tree.size-1, pos, value) }
-
-func GetIntTree(size int) Tree    { return intTree{getEmpty(size, IntValue(0)), size} }
-func GetFloatTree(size int) Tree  { return floatTree{getEmpty(size, FloatValue(0)), size} }
-func GetStringTree(size int) Tree { return stringTree{getEmpty(size, StringValue("")), size} }
+func GetIntTree(size int) Tree    { return segmentTree{getEmpty(size, IntValue(0)), size} }
+func GetStringTree(size int) Tree { return segmentTree{getEmpty(size, StringValue("")), size} }
+func GetFloatTree(size int) Tree  { return segmentTree{getEmpty(size, FloatValue(0)), size} }
