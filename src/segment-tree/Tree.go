7fb70a825b34c83123f53143abaaf13f7ef2f5f1
olegfomenko <oleg.fomenko2002@gmail.com>
2020-12-03 22:46:12 +0200

    Refactoring segment-tree code (moving functions)

--- src/segment-tree/Tree.go
+++ src/segment-tree/Tree.go
@@ -7,6 +7,12 @@ type Tree interface {
 
 	// Change value on certain pos
 	Assign(value Value, pos int)
+
+	// Inner get function that contains main segment tree get-on-segment logic
+	get(v int, tl int, tr int, l int, r int) Value
+
+	// Inner update function that contains main segment tree re-assign logic
+	update(v int, tl int, tr int, pos int, value Value)
 }
 
 type segmentTree struct {
@@ -15,11 +21,48 @@ type segmentTree struct {
 }
 
 func (tree segmentTree) Get(l int, r int) Value {
-	return get(tree.t, 1, 0, tree.size-1, l, r)
+	return tree.get(1, 0, tree.size-1, l, r)
 }
 
 func (tree segmentTree) Assign(value Value, pos int) {
-	update(tree.t, 1, 0, tree.size-1, pos, value)
+	tree.update(1, 0, tree.size-1, pos, value)
+}
+
+func (tree segmentTree) get(v int, tl int, tr int, l int, r int) Value {
+	if l > r {
+		return nil
+	}
+
+	if l == tl && r == tr {
+		return tree.t[v]
+	}
+
+	var tm int = (tl + tr) / 2
+
+	left := tree.get(v*2, tl, tm, l, min(r, tm))
+	right := tree.get(v*2+1, tm+1, tr, max(l, tm+1), r)
+
+	if left == nil {
+		return right
+	} else {
+		return left.Add(right)
+	}
+}
+
+func (tree segmentTree) update(v int, tl int, tr int, pos int, value Value) {
+	if tl == tr {
+		tree.t[v] = value
+	} else {
+		var tm int = (tl + tr) / 2
+
+		if pos <= tm {
+			tree.update(v*2, tl, tm, pos, value)
+		} else {
+			tree.update(v*2+1, tm+1, tr, pos, value)
+		}
+
+		tree.t[v] = tree.t[v*2].Add(tree.t[v*2+1])
+	}
 }
 
 func GetTree(size int, defaultValue Value) Tree {
---
+++ src/segment-tree/aditional.go
@@ -0,0 +1,27 @@
+package segment_tree
+
+func min(x int, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func max(x int, y int) int {
+	if x > y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func getEmpty(size int, def Value) []Value {
+	values := make([]Value, 4*size+4)
+
+	for i := 0; i < 4*size+4; i++ {
+		values = append(values, def)
+	}
+
+	return values
+}
